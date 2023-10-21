6e8520ffd351ea6fab5ec9872ef75c01dcd6119d
Mona Lisa <pranavjayaprakashn98@gmail.com>
2023-10-21 15:48:40 +0530

    update

---
+++ adminside/src/Components/AdminSign.jsx
@@ -0,0 +1,16 @@
+import React,{useState} from 'react'
+
+function AdminSign() {
+    const [username, setusername] = useState('')
+    const [email, setemail] = useState('')
+    const [password, setpassword] = useState('')
+    const [address,setaddress]=useState('')
+    
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default AdminSign
