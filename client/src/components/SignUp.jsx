5f7ed3e4aca79e80d9c47dcb71af1c17b5c20298
Mona Lisa <pranavjayaprakashn98@gmail.com>
2023-10-29 20:52:00 +0530

    update

--- client/src/components/SignUp.jsx
+++ client/src/components/SignUp.jsx
@@ -1,53 +1,65 @@
-import React,{useState} from 'react'
-import { signUpdata } from './Api'
-import { Link } from 'react-router-dom'
-
+import React, { useState } from "react";
+import { signUpdata } from "./Api";
+import { Link } from "react-router-dom";
 
 function SignUp() {
+  const [username, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [address, setAddress] = useState("");
+  const [password, setpassword] = useState("");
+  const [image, setimage] = useState({});
 
-    const [username, setName] = useState('')
-    const [email, setEmail] = useState('')
-    const [address, setAddress] = useState('')
-    const [password, setpassword] = useState('')
-    const [image,setimage] = useState({})
+  const formdata = new FormData();
 
-
-
-
-    const formdata = new FormData()
-
-    formdata.append("username",username)
-    formdata.append("email",email)
-    formdata.append("address",address)
-    formdata.append("password",password)
-    formdata.append("Images",image)
-const display=(e)=>{
-    e.preventDefault()
-    signUpdata(formdata)
-
-
-}
+  formdata.append("username", username);
+  formdata.append("email", email);
+  formdata.append("address", address);
+  formdata.append("password", password);
+  formdata.append("Images", image);
+  const display = (e) => {
+    e.preventDefault();
+    signUpdata(formdata);
+  };
   return (
     <div>
-        <form onSubmit={display}>
-            <input type="text" placeholder='username' value={username} onChange={(e)=>setName(e.target.value)}/>
-            <input type="text" placeholder='email' value={email} onChange={(e)=>setEmail(e.target.value)}/>
-            <input type="text" placeholder='address' value={address} onChange={(e)=>setAddress(e.target.value)}/>
-            <input type='file' filename='Images' onChange={(e)=>setimage(e.target.files[0])}/>
-            <input type="text" placeholder='password' value={password} onChange={(e)=>setpassword(e.target.value)}/>
-            <input type="submit" />
-        </form>
+      <form onSubmit={display}>
+        <input
+          type="text"
+          placeholder="username"
+          value={username}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="address"
+          value={address}
+          onChange={(e) => setAddress(e.target.value)}
+        />
+        <input
+          type="file"
+          filename="Images"
+          onChange={(e) => setimage(e.target.files[0])}
+        />
+        <input
+          type="text"
+          placeholder="password"
+          value={password}
+          onChange={(e) => setpassword(e.target.value)}
+        />
+        <input type="submit" />
+      </form>
 
-
-
-    <Link to={'/'}>
-        <div>
-            Alread have an account?
-        </div>
-        </Link>
-        
+      <Link to={"/"}>
+        <div>Alread have an account?</div>
+      </Link>
     </div>
-  )
+  );
 }
 
-export default SignUp
+export default SignUp;
