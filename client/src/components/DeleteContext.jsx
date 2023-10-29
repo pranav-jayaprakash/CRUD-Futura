697b23b56e29dc6f8d39a9bb0bb8b6f7b4333f8d
Mona Lisa <pranavjayaprakashn98@gmail.com>
2023-10-29 21:00:11 +0530

    update

--- client/src/components/DeleteContext.jsx
+++ client/src/components/DeleteContext.jsx
@@ -1,33 +1,28 @@
-import React from 'react'
-import { useSelector,useDispatch } from 'react-redux'
-import { DeleteData } from './Api'
-import { Logout } from '../reddux/Dataslice'
-
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { DeleteData } from "./Api";
+import { Logout } from "../reddux/Dataslice";
 
 function DeleteContext() {
+  const dispatch = useDispatch();
 
-    const dispatch=useDispatch()
+  var Currentdata = useSelector((state) => state.Userdata.loginData);
+  console.log("current data", Currentdata);
 
-    var Currentdata = useSelector(state=>state.Userdata.loginData)
-  console.log("current data",Currentdata)
+  const iddata = Currentdata[0] && Currentdata[0]._id;
 
-  const iddata= Currentdata[0]&&Currentdata[0]._id
-
-  const Delac =()=>{
-    DeleteData(iddata)
-    dispatch(Logout())
-    
-  }
-
+  const Delac = () => {
+    DeleteData(iddata);
+    dispatch(Logout());
+  };
 
   return (
     <div>
-        <h3>You are about delete your account!!!!
-        </h3>
-        <h2>Are you sure?</h2>
-        <button onClick={Delac}>Delete My Account</button>
+      <h3>You are about delete your account!!!!</h3>
+      <h2>Are you sure?</h2>
+      <button onClick={Delac}>Delete My Account</button>
     </div>
-  )
+  );
 }
 
-export default DeleteContext
+export default DeleteContext;
