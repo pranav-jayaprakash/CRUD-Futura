a59e88d2ba9774e52abf2f04a4cb3353bf41addf
Mona Lisa <pranavjayaprakashn98@gmail.com>
2023-10-29 21:04:42 +0530

    update

--- client/src/components/Api.js
+++ client/src/components/Api.js
@@ -1,57 +1,48 @@
-import axios from 'axios'
-import { Pushdata } from '../reddux/Dataslice'
-import { publucRequest, userRequest } from '../Requestmethod'
-export const signUpdata =async (data)=>{
+import axios from "axios";
+import { Pushdata } from "../reddux/Dataslice";
+import { publucRequest, userRequest } from "../Requestmethod";
+export const signUpdata = async (data) => {
+  console.log(data, "initial data");
 
-    console.log(data, 'initial data')
+  try {
+    const res = await publucRequest.post("/", data);
+    console.log("Final data", res.data);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-    try {
+export const LoginData = async (data, dispatch) => {
+  console.log("Data", data);
+  try {
+    const res = await publucRequest.post("/login", data);
+    console.log("Final daata", res.data);
+    dispatch(Pushdata(res.data));
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-        const res = await publucRequest.post('/',data)
-        console.log("Final data",res.data)
+export const UpdateData = async (data, id) => {
+  console.log(data, id, "Got data");
+  try {
+    const Upd = await userRequest.put(
+      `http://localhost:8001/api/update/${id}`,
+      data
+    );
+    console.log(Upd, "Updated data");
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-
-        
-    } catch (error) {
-        console.log(error)
-        
-    }
-}
-
-export const LoginData = async (data,dispatch)=>{
-    console.log("Data",data)
-    try {
-
-        const res = await publucRequest.post('/login',data)
-        console.log("Final daata",res.data)
-        dispatch(Pushdata(res.data))
-        
-    } catch (error) {
-        console.log(error)
-    }
-}
-
-export const UpdateData = async (data,id)=>{
-
-    console.log(data,id,'Got data')
-    try {
-        const Upd= await userRequest.put(`http://localhost:8001/api/update/${id}`,data)
-        console.log(Upd,'Updated data')
-        
-    } catch (error) {
-        console.log(error)
-        
-    }
-}
-
-export const DeleteData = async (id)=>{
-    try {
-
-        const del= await userRequest.delete(`http://localhost:8001/api/update/delete/${id}`)
-        console.log(del)
-        
-    } catch (error) {
-        console.log(error)
-        
-    }
-}
+export const DeleteData = async (id) => {
+  try {
+    const del = await userRequest.delete(
+      `http://localhost:8001/api/update/delete/${id}`
+    );
+    console.log(del);
+  } catch (error) {
+    console.log(error);
+  }
+};
